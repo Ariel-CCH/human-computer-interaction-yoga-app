b744188c7d9982d0e21fadd06dec6c22f93df189
chiahsun <65946749+Ariel-CCH@users.noreply.github.com>
2021-09-11 21:14:54 -0400

    Update script.js

--- hci-portfolio-em/JS/script.js
+++ hci-portfolio-em/JS/script.js
@@ -8,20 +8,24 @@
 	$(".step01").click( function() {
 		$("#line-progress").css("width", "10%");
 		$("#intro").addClass("active").siblings().removeClass("active");
+		document.body.scrollTop = document.documentElement.scrollTop=0;
 	});
 	
 	$(".step02").click( function() {
 		$("#line-progress").css("width", "38%");
 		$("#data").addClass("active").siblings().removeClass("active");
+		document.body.scrollTop = document.documentElement.scrollTop=0;
 	});
 	
 	$(".step03").click( function() {
 		$("#line-progress").css("width", "69%");
 		$("#develop").addClass("active").siblings().removeClass("active");
+		document.body.scrollTop = document.documentElement.scrollTop=0;
 	});
 	
 	$(".step04").click( function() {
 		$("#line-progress").css("width", "100%");
 		$("#conclusion").addClass("active").siblings().removeClass("active");
+		document.body.scrollTop = document.documentElement.scrollTop=0;
 	});
 	
